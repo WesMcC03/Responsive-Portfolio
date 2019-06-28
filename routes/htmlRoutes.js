239edab4e57247d00822a66547c9597c81a00bca
Wes McC <wkmccowin@yahoo.com>
2019-06-28 15:02:28 -0700

    Added server

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,27 @@
+var path =require('path');
+
+module.exports = function(app) {
+    app.get("/home", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+
+    });
+    
+
+    app.get("/contact", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+
+    });
+
+    app.get("/portfolio", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+
+    });
+  
+    
+    
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      });
+
+
+};
---
+++ server.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+var express = require("express");
+// var exphbs = require("express-handlebars");
+var path = require("path");
+
+// var db = require("./models");
+
+var app = express();
+
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+console.log(__dirname);
+
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+// Routes
+
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+var PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+module.exports = app;
